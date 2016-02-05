7fbc33db3ea8d2a94de05bd27ea87e851c169cc8
Charles Kubicek <charles.kubicek@digital.hmrc.gov.uk>
2016-02-05 11:26:55 +0000

    working hello-world endpoints

--- app/uk/gov/hmrc/catalogue/config/microserviceWiring.scala
+++ app/uk/gov/hmrc/catalogue/config/microserviceWiring.scala
@@ -14,22 +14,12 @@
  * limitations under the License.
  */
 
-package uk.gov.hmrc.catalogue.controllers.config
+package uk.gov.hmrc.catalogue.config
 
-import uk.gov.hmrc.play.audit.http.config.LoadAuditingConfig
-import uk.gov.hmrc.play.audit.http.connector.AuditConnector
-import uk.gov.hmrc.play.auth.microservice.connectors.AuthConnector
-import uk.gov.hmrc.play.config.{AppName, RunMode, ServicesConfig}
+import uk.gov.hmrc.play.config.{AppName, RunMode}
+import uk.gov.hmrc.play.http.hooks.HttpHook
 import uk.gov.hmrc.play.http.ws._
 
 object WSHttp extends WSGet with WSPut with WSPost with WSDelete with WSPatch with AppName with RunMode {
-  override val auditConnector = MicroserviceAuditConnector
-}
-
-object MicroserviceAuditConnector extends AuditConnector with RunMode {
-  override lazy val auditingConfig = LoadAuditingConfig(s"$env.auditing")
-}
-
-object MicroserviceAuthConnector extends AuthConnector with ServicesConfig {
-  override val authBaseUrl = baseUrl("auth")
+  override val hooks: Seq[HttpHook] = Seq()
 }
