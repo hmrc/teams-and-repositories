257f896e1d4612782af36bca3d8af9048a9c970a
Pete Smith <pete.smith@digital.hmrc.gov.uk>
2016-05-17 14:49:41 +0100

    [PS] Services endpoints will now consider a pair of one open, and one internal repository of the same name as a single service, providing links to both repositories but only considering open for CI

--- test/uk/gov/hmrc/teamsandservices/DataSourceToApiContractMappingsSpec.scala
+++ test/uk/gov/hmrc/teamsandservices/DataSourceToApiContractMappingsSpec.scala
@@ -20,7 +20,7 @@ import org.scalatest.{Matchers, OptionValues, WordSpec}
 import uk.gov.hmrc.teamsandservices.config.{UrlTemplate, UrlTemplates}
 import uk.gov.hmrc.teamsandservices.DataSourceToApiContractMappings._
 
-class ServiceSpec extends WordSpec with Matchers with OptionValues {
+class DataSourceToApiContractMappingsSpec extends WordSpec with Matchers with OptionValues {
 
   val urlTemplates = UrlTemplates(
     ciOpen = Seq(UrlTemplate(
@@ -33,35 +33,59 @@ class ServiceSpec extends WordSpec with Matchers with OptionValues {
     ))
   )
 
-  "Services" should {
+  "Mapping between repositories and services" should {
 
     "create links for a closed service" in {
 
-      val repo = Repository(
+      val repos = Seq(Repository(
         "a-frontend",
         "https://not-open-github/org/a-frontend",
         isInternal = true,
-        deployable = true)
+        deployable = true))
 
-      repo.asService(Seq("teamName"), urlTemplates).get shouldBe Service(
+      repos.asService(Seq("teamName"), urlTemplates).get shouldBe Service(
         "a-frontend",
         Seq("teamName"),
-        Link("github", "https://not-open-github/org/a-frontend"),
+        Seq(Link("github", "https://not-open-github/org/a-frontend")),
         List(Link("closed", "http://closed/a-frontend"))
       )
     }
 
     "create links for a open service" in {
 
-      val repo = Repository(
+      val repo = Seq(Repository(
         "a-frontend",
         "https://github.com/org/a-frontend",
-        deployable = true)
+        deployable = true))
 
       repo.asService(Seq("teamName"), urlTemplates).value shouldBe Service(
         "a-frontend",
         Seq("teamName"),
-        Link("github", "https://github.com/org/a-frontend"),
+        Seq(Link("github-open", "https://github.com/org/a-frontend")),
+        List(Link("open", "http://open/a-frontend"))
+      )
+    }
+
+    "create github links for both open and internal services if both are present, but only open ci links" in {
+
+      val internalRepo = Repository(
+        "a-frontend",
+        "https://not-open-github/org/a-frontend",
+        isInternal = true,
+        deployable = true)
+
+      val openRepo = Repository(
+        "a-frontend",
+        "https://github.com/org/a-frontend",
+        deployable = true)
+
+      val repos = Seq(internalRepo, openRepo)
+      repos.asService(Seq("teamName"), urlTemplates).value shouldBe Service(
+        "a-frontend",
+        Seq("teamName"),
+        Seq(
+          Link("github", "https://not-open-github/org/a-frontend"),
+          Link("github-open", "https://github.com/org/a-frontend")),
         List(Link("open", "http://open/a-frontend"))
       )
     }
