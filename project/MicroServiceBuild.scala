cd8ce6d4e4a8a291d45f1e990b0f9ca827cdcea3
ashu.singh <asingh@equalexperts.com>
2016-11-03 10:51:07 +0000

    upgraded play json logger

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -21,7 +21,7 @@ private object AppDependencies {
   private val microserviceBootstrapVersion = "5.1.0"
   private val playAuthVersion = "4.0.0"
   private val playHealthVersion = "2.0.0"
-  private val logbackJsonLoggerVersion = "3.0.0"
+  private val playJsonLoggerVersion = "3.0.0"
   private val playUrlBindersVersion = "2.0.0"
   private val playConfigVersion = "3.0.0"
   private val domainVersion = "3.3.0"
@@ -36,7 +36,7 @@ private object AppDependencies {
     "uk.gov.hmrc" %% "play-health" % playHealthVersion,
     "uk.gov.hmrc" %% "play-url-binders" % playUrlBindersVersion,
     "uk.gov.hmrc" %% "play-config" % playConfigVersion,
-    "uk.gov.hmrc" %% "logback-json-logger" % logbackJsonLoggerVersion,
+    "uk.gov.hmrc" %% "play-json-logger" % playJsonLoggerVersion,
     "uk.gov.hmrc" %% "domain" % domainVersion,
     "org.mockito" % "mockito-core" % "2.2.6",
     "org.apache.httpcomponents" % "httpcore" % "4.3.2",
