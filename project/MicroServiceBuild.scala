d93f14ff2243146b7b7dbbcb61417f2399a36769
Armin Keyvanloo <armin.keyvanloo@gmail.com>
2017-09-05 14:26:00 +0100

    [PLATOPS-1060] upgrading the microservice-bootstrap to 6.8.0 (fixes the logging on the server)

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -18,7 +18,7 @@ private object AppDependencies {
 
   import play.core.PlayVersion.current
 
-  private val microserviceBootstrapVersion = "6.7.0"
+  private val microserviceBootstrapVersion = "6.8.0"
   private val playUrlBindersVersion = "2.1.0"
   private val domainVersion = "4.1.0"
   private val hmrcTestVersion = "2.3.0"
