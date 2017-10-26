433fac0bda6a72dda1fd0784c33f9035b90a9822
Armin Keyvanloo <armin.keyvanloo@gmail.com>
2017-10-26 14:21:35 +0100

    [PLATOPS-1114] renaming FiltersWithCORS to Filters

--- app/uk/gov/hmrc/teamsandrepositories/Filters.scala
+++ app/uk/gov/hmrc/teamsandrepositories/Filters.scala
@@ -6,5 +6,5 @@ import play.api.http.DefaultHttpFilters
 import play.filters.cors.CORSFilter
 import uk.gov.hmrc.play.bootstrap.filters.MicroserviceFilters
 
-class FiltersWithCORS @Inject()(defaultFilters : MicroserviceFilters, corsFilter: CORSFilter)
+class Filters @Inject()(defaultFilters : MicroserviceFilters, corsFilter: CORSFilter)
   extends DefaultHttpFilters(defaultFilters.filters :+ corsFilter: _*)
