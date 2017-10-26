300c03f3f6973801f729ee07a7f04feeeccfc6c2
Armin Keyvanloo <armin.keyvanloo@gmail.com>
2017-10-26 09:16:50 +0100

    [PLATOPS-1114] enabling CORS

---
+++ app/uk/gov/hmrc/teamsandrepositories/Filters.scala
@@ -0,0 +1,10 @@
+package uk.gov.hmrc.teamsandrepositories
+
+import javax.inject.Inject
+
+import play.api.http.DefaultHttpFilters
+import play.filters.cors.CORSFilter
+import uk.gov.hmrc.play.bootstrap.filters.MicroserviceFilters
+
+class Filters @Inject() (defaultFilters : MicroserviceFilters, corsFilter: CORSFilter)
+  extends DefaultHttpFilters(defaultFilters.filters :+ corsFilter: _*)
