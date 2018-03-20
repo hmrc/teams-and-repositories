580d31f2317d6290b9abe7026712a2d9ca417b24
Konrad <wudkowski@gmail.com>
2018-03-20 15:04:58 +0000

    Routine library upgrade

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -20,22 +20,16 @@ private object AppDependencies {
 
   import play.core.PlayVersion.current
 
-  private val playUrlBindersVersion    = "2.1.0"
-  private val domainVersion            = "4.1.0"
-  private val hmrcTestVersion          = "2.3.0"
-  private val githubClientVersion      = "1.18.0"
-  private val playReactivemongoVersion = "6.0.0"
-
   val compile = Seq(
-    "uk.gov.hmrc"               %% "bootstrap-play-25"  % "0.7.0",
-    "uk.gov.hmrc"               %% "github-client"      % githubClientVersion,
-    "uk.gov.hmrc"               %% "play-url-binders"   % playUrlBindersVersion,
-    "uk.gov.hmrc"               %% "domain"             % domainVersion,
+    "uk.gov.hmrc"               %% "bootstrap-play-25"  % "1.5.0",
+    "uk.gov.hmrc"               %% "github-client"      % "1.18.0",
+    "uk.gov.hmrc"               %% "play-url-binders"   % "2.1.0",
+    "uk.gov.hmrc"               %% "domain"             % "4.1.0",
     "org.yaml"                  % "snakeyaml"           % "1.17",
     "org.apache.httpcomponents" % "httpcore"            % "4.3.2",
     "org.apache.httpcomponents" % "httpclient"          % "4.3.5",
     "uk.gov.hmrc"               %% "mongo-lock"         % "5.0.0",
-    "uk.gov.hmrc"               %% "play-reactivemongo" % playReactivemongoVersion
+    "uk.gov.hmrc"               %% "play-reactivemongo" % "6.2.0"
   )
 
   trait TestDependencies {
@@ -47,13 +41,13 @@ private object AppDependencies {
     def apply() =
       new TestDependencies {
         override lazy val test = Seq(
-          "uk.gov.hmrc"            %% "hmrctest"           % hmrcTestVersion % scope,
-          "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.0"         % scope,
-          "org.pegdown"            % "pegdown"             % "1.4.2"         % scope,
-          "com.typesafe.play"      %% "play-test"          % current         % scope,
-          "uk.gov.hmrc"            %% "reactivemongo-test" % "2.0.0"         % scope,
-          "com.github.tomakehurst" % "wiremock"            % "1.52"          % scope,
-          "org.mockito"            % "mockito-core"        % "2.3.5"         % scope
+          "uk.gov.hmrc"            %% "hmrctest"           % "2.3.0" % scope,
+          "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.0" % scope,
+          "org.pegdown"            % "pegdown"             % "1.4.2" % scope,
+          "com.typesafe.play"      %% "play-test"          % current % scope,
+          "uk.gov.hmrc"            %% "reactivemongo-test" % "2.0.0" % scope,
+          "com.github.tomakehurst" % "wiremock"            % "1.52"  % scope,
+          "org.mockito"            % "mockito-core"        % "2.3.5" % scope
         )
       }.test
   }
