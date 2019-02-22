07930458c1b48eaa8c485b5f5368416cd82ef9ad
Dario Abdulrehman <167463+dabd@users.noreply.github.com>
2019-02-22 09:51:19 +0000

    PLATOPS-1850: Library security upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,25 +5,25 @@ object AppDependencies {
   import play.core.PlayVersion.current
   import play.sbt.PlayImport._
 
-  val bootstrapPlayVersion = "0.23.0"
+  val bootstrapPlayVersion = "0.36.0"
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc"               %% "bootstrap-play-26"  % bootstrapPlayVersion,
-    "uk.gov.hmrc"               %% "github-client"      % "2.5.0",
-    "org.yaml"                  % "snakeyaml"           % "1.17",
-    "org.apache.httpcomponents" % "httpcore"            % "4.3.2",
-    "org.apache.httpcomponents" % "httpclient"          % "4.3.5",
-    "uk.gov.hmrc"               %% "mongo-lock"         % "6.1.0-play-26"
+    "uk.gov.hmrc"               %% "bootstrap-play-26" % bootstrapPlayVersion,
+    "uk.gov.hmrc"               %% "github-client"     % "2.8.0",
+    "org.yaml"                  % "snakeyaml"          % "1.17",
+    "org.apache.httpcomponents" % "httpcore"           % "4.3.2",
+    "org.apache.httpcomponents" % "httpclient"         % "4.3.5",
+    "uk.gov.hmrc"               %% "mongo-lock"        % "6.10.0-play-26"
   )
   val test = Seq(
     "uk.gov.hmrc"            %% "bootstrap-play-26"  % bootstrapPlayVersion % Test classifier "tests",
-    "uk.gov.hmrc"            %% "hmrctest"           % "3.2.0"              % Test,
-    "org.scalatest"          %% "scalatest"          % "3.0.4"              % Test,
+    "uk.gov.hmrc"            %% "hmrctest"           % "3.3.0"              % Test,
+    "org.scalatest"          %% "scalatest"          % "3.0.5"              % Test,
     "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2"              % Test,
     "org.pegdown"            % "pegdown"             % "1.5.0"              % Test,
     "com.typesafe.play"      %% "play-test"          % current              % Test,
-    "uk.gov.hmrc"            %% "reactivemongo-test" % "4.1.0-play-26"      % Test,
+    "uk.gov.hmrc"            %% "reactivemongo-test" % "4.8.0-play-26"      % Test,
     "com.github.tomakehurst" % "wiremock"            % "1.52"               % Test,
     "org.mockito"            % "mockito-core"        % "2.3.5"              % Test
   )
