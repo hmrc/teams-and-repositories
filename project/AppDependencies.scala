8d892d6dc9ee4564d79609ad8bfc92da6369d7c2
Tim Bennett <2475478+bennetimo@users.noreply.github.com>
2020-01-02 14:09:13 +0000

    BDOG-545 update build and plugins to sbt 1.3.4

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,15 +5,15 @@ object AppDependencies {
   import play.core.PlayVersion.current
   import play.sbt.PlayImport._
 
-  val bootstrapPlayVersion = "0.36.0"
+  val bootstrapPlayVersion = "0.46.0"
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc"               %% "bootstrap-play-26" % bootstrapPlayVersion,
-    "uk.gov.hmrc"               %% "github-client"     % "2.11.0",
-    "org.yaml"                  % "snakeyaml"          % "1.17",
-    "uk.gov.hmrc"               %% "mongo-lock"        % "6.10.0-play-26",
-    "org.typelevel"             %% "cats-core"         % "2.0.0"
+    "uk.gov.hmrc"            %% "bootstrap-play-26" % bootstrapPlayVersion,
+    "uk.gov.hmrc"            %% "github-client"     % "2.11.0",
+    "org.yaml"               % "snakeyaml"          % "1.17",
+    "uk.gov.hmrc"            %% "mongo-lock"        % "6.10.0-play-26",
+    "org.typelevel"          %% "cats-core"         % "2.0.0"
   )
   val test = Seq(
     "uk.gov.hmrc"            %% "bootstrap-play-26"  % bootstrapPlayVersion % Test classifier "tests",
@@ -23,7 +23,7 @@ object AppDependencies {
     "org.pegdown"            % "pegdown"             % "1.5.0"              % Test,
     "com.typesafe.play"      %% "play-test"          % current              % Test,
     "uk.gov.hmrc"            %% "reactivemongo-test" % "4.8.0-play-26"      % Test,
-    "com.github.tomakehurst" % "wiremock"            % "1.52"               % Test,
+    "com.github.tomakehurst" % "wiremock"            % "1.58"               % Test,
     "org.mockito"            % "mockito-core"        % "2.3.5"              % Test
   )
 }
