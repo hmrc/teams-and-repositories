d5e66d6df4401db72c29ce78192dda651508c64f
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2020-06-08 10:58:20 +0100

    BDOG-391: Persisting archived flag to response from api for respositories and removed from other endpoints

--- test/uk/gov/hmrc/teamsandrepositories/TeamsAndReposPersisterSpec.scala
+++ test/uk/gov/hmrc/teamsandrepositories/TeamsAndReposPersisterSpec.scala
@@ -19,7 +19,6 @@ package uk.gov.hmrc.teamsandrepositories
 import com.codahale.metrics.MetricRegistry
 import com.kenshoo.play.metrics.Metrics
 import org.mockito.Mockito._
-import org.mockito.ArgumentMatchers.any
 import org.scalatest.{BeforeAndAfterEach, LoneElement, OptionValues}
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.matchers.should.Matchers
