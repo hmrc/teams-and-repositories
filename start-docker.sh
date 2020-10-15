bc74fd57e081364a24b5040a0c95946b2c25074e
anshulbajpai <291429+anshulbajpai@users.noreply.github.com>
2020-10-15 09:18:26 +0100

    BDOG-1068: Upgrading jvm-bobby

--- start-docker.sh
+++ start-docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-export JVM_BOBBY_VERSION=0.3.0
+export JVM_BOBBY_VERSION=0.4.0
 export JVM_BOBBY_URL="https://artefacts.tax.service.gov.uk/artifactory/hmrc-releases-local/uk/gov/hmrc/jvm-bobby/${JVM_BOBBY_VERSION}/jvm-bobby-${JVM_BOBBY_VERSION}-assembly.jar"
 curl --location --noproxy "discoverd" --retry 5 "${JVM_BOBBY_URL}" -o "jvm-bobby.jar"
 export JAVA_OPTS="$JAVA_OPTS -javaagent:${PWD}/jvm-bobby.jar"
