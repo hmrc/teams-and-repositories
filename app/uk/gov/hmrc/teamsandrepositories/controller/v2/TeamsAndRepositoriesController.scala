d09d9ffe0cf059c6e72cd960d1aaa531839dccde
Sam Sloman <samsloman@live.co.uk>
2022-08-12 10:13:33 +0100

    BDOG-1263: Adds service-configs connector

--- app/uk/gov/hmrc/teamsandrepositories/controller/v2/TeamsAndRepositoriesController.scala
+++ app/uk/gov/hmrc/teamsandrepositories/controller/v2/TeamsAndRepositoriesController.scala
@@ -20,6 +20,7 @@ import play.api.libs.json.{JsError, JsValue, Json, Reads}
 import play.api.mvc.{Action, ControllerComponents}
 import uk.gov.hmrc.internalauth.client.{BackendAuthComponents, IAAction, Predicate, Resource}
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+import uk.gov.hmrc.teamsandrepositories.connectors.ServiceConfigsConnector
 import uk.gov.hmrc.teamsandrepositories.models.{GitRepository, RepoType, TeamName}
 import uk.gov.hmrc.teamsandrepositories.persistence.RepositoriesPersistence
 import uk.gov.hmrc.teamsandrepositories.services.BranchProtectionService
@@ -29,10 +30,11 @@ import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
 class TeamsAndRepositoriesController @Inject()(
-  repositoriesPersistence: RepositoriesPersistence,
-  branchProtectionService: BranchProtectionService,
-  auth: BackendAuthComponents,
-  cc: ControllerComponents
+                                                repositoriesPersistence: RepositoriesPersistence,
+                                                branchProtectionService: BranchProtectionService,
+                                                frontendServicesConnector: ServiceConfigsConnector,
+                                                auth: BackendAuthComponents,
+                                                cc: ControllerComponents
 )(implicit
   ec: ExecutionContext
 ) extends BackendController(cc) {
