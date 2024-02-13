b8d0300c10a32dd91442be6689a34d4d06fee261
peteanning <22272449+peteanning@users.noreply.github.com>
2024-02-13 17:26:46 +0000

    BDOG-2839: add test only endpoint for teamSummaries

--- app/uk/gov/hmrc/teamsandrepositories/persistence/TeamSummaryPersistence.scala
+++ app/uk/gov/hmrc/teamsandrepositories/persistence/TeamSummaryPersistence.scala
@@ -16,6 +16,7 @@
 
 package uk.gov.hmrc.teamsandrepositories.persistence
 
+import org.mongodb.scala.bson.BsonDocument
 import org.mongodb.scala.model.Indexes.ascending
 import org.mongodb.scala.model.{DeleteOneModel, Filters, IndexModel, IndexOptions, Indexes, ReplaceOneModel, ReplaceOptions}
 import play.api.Logger
@@ -66,4 +67,17 @@ class TeamSummaryPersistence @Inject()(
       .find()
       .sort(ascending("name"))
       .toFuture()
+
+  def addTeamSummaries(teams: Seq[TeamSummary]): Future[Unit] =
+    collection
+      .insertMany(teams)
+      .toFuture()
+      .map(_ => ())
+
+  def deleteAll(): Future[Unit] =
+   collection
+     .deleteMany(BsonDocument())
+     .toFuture()
+     .map(_ => ())
+
 }
