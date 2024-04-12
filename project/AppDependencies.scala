1287e9d2bf956b3e14605c0e583c09b70de511b6
colin-lamed <9568290+colin-lamed@users.noreply.github.com>
2024-04-12 16:07:20 +0100

    BDOG-3107 Downgrade hmrc-mongo to confirm behaviour

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 
 object AppDependencies {
   val bootstrapPlayVersion = "8.5.0"
-  val hmrcMongoVersion     = "1.8.0"
+  val hmrcMongoVersion     = "1.7.0"
 
   val compile = Seq(
     ws,
