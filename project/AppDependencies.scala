aabecf8000073376597025fc6451a8a5339652c2
Shnick <1132919+Shnick@users.noreply.github.com>
2025-03-13 14:55:20 +0000

    BDOG-3401 add External to Tags enum

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,14 +2,14 @@ import play.sbt.PlayImport.ws
 import sbt._
 
 object AppDependencies {
-  val bootstrapPlayVersion = "9.10.0"
-  val hmrcMongoVersion     = "2.5.0"
+  val bootstrapPlayVersion = "9.11.0"
+  val hmrcMongoVersion     = "2.6.0"
 
   val compile = Seq(
     ws,
     "uk.gov.hmrc"            %% "bootstrap-backend-play-30"    % bootstrapPlayVersion,
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-metrix-play-30"    % hmrcMongoVersion,
-    "uk.gov.hmrc"            %% "internal-auth-client-play-30" % "3.0.0",
+    "uk.gov.hmrc"            %% "internal-auth-client-play-30" % "3.1.0",
     "org.yaml"               %  "snakeyaml"                    % "2.3",
     "org.typelevel"          %% "cats-core"                    % "2.13.0",
     "org.codehaus.groovy"    %  "groovy-astbuilder"            % "3.0.16",
